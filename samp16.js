f0fa1caf48633c4c38bcf212d06efa1bc6d5521b
roseni94 <anroseni2003@gmail.com>
2023-02-06 06:32:17 +0530

    first commit

---
+++ samp16.js
@@ -0,0 +1,34 @@
+@media (max-width: 799px)
+.Tech_enzyme__2R0v_, .Tech_ethereum__lymjN {
+    margin-bottom: 50px;
+}
+.Tech_ethereum__lymjN {
+    display: block;
+    background-blend-mode: darken,normal;
+    margin: 0 30px;
+    width: 197px;
+    height: 49px;
+    background: url(/logos/ethereum-default.png) no-repeat 50%;
+    background-size: contain;
+}
+a {
+    color: inherit;
+    text-decoration: none;
+}
+* {
+    box-sizing: border-box;
+}
+user agent stylesheet
+a:-webkit-any-link {
+    color: -webkit-link;
+    cursor: pointer;
+    text-decoration: underline;
+}
+body, html {
+    display: flex;
+    flex-direction: column;
+    margin: 0;
+    font-family: Poppins,sans-serif;
+    background: #0f0d1e;
+    color: #fff;
+}
